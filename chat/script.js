44712baa50eff487d80afdd8ecabe914e8e4213f
QuantumKya <rohan.vats4nier@gmail.com>
2025-03-28 01:42:15 -0500

    fixed the website, deleted all of the horrid failures :walc:

--- chat/script.js
+++ chat/script.js
@@ -1,33 +1,41 @@
-async function getChatData(url) {
-    let dataIn;
-    fetch(url)
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+const pullurl = "https://qk-vercel-fastapi.vercel.app/sent/";
+const sendurl = "https://qk-vercel-fastapi.vercel.app/yap/";
+let shouldUpdate = false;
 
-    return dataIn;
+async function getMessages() {
+    try {
+        const res = await fetch(pullurl);
+        const data = res.json();
+        return data;
+    }
+    catch (error) { console.log(error); }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+async function yapMessage(message, sender) {
+    await fetch(sendurl, {
+        method: "POST",
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify({"message": message, "sender": sender})
+    });
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
     const chatDisplayEl = document.getElementById('chat-display');
     const messageFormEl = document.getElementById('textbox');
     const messageInputEl = document.getElementById('message-box');
     const messageSendEl = document.getElementById('send-button');
 
-    const targeturl = "https://qk-vercel-fastapi.vercel.app/sent/";
-
-    getChatData(targeturl);
-
-    /*
-    while (true) {
-        const currentChat = getChatData(targeturl);
+    async function updateChat() {
+        let currentChat = await getMessages();
         console.log(currentChat);
-        chatDisplayEl.innerHTML = '';
-        for (x of JSON.parse(currentChat)) {
+        
+        chatDisplayEl.textContent = '';
+        for (let i = 0; i < currentChat.length; i++) {
             let newL = document.createElement('li');
-            newL.innerHTML = `${x["sender"]}: ${x["message"]}`;
+            newL.innerHTML = `${currentChat[i]["sender"]}: ${currentChat[i]["message"]}`;
             chatDisplayEl.append(newL);
         }
     }
-    */
+
+    updateChat();
 });
