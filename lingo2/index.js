1a9378c5252bbcb6b5dd9e43e9014f810d47a707
QuantumKya <rohan.vats4nier@gmail.com>
2025-04-07 17:15:05 -0500

    added golgi

--- lingo2/index.js
+++ lingo2/index.js
@@ -94,7 +94,7 @@ setNames(
 
     "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
 
-    "katar", "parabox", "halo", "tuna", "cymbal", "quatrefoil", "linkbetweenwords", "poketoads"
+    "katar", "parabox", "halo", "tuna", "cymbal", "quatrefoil", "linkbetweenwords", "poketoads", "golgiyoshi"
 );
 
 function getOffset(i) {
